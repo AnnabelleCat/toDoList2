a0bb3d219fadfa2b84f3e32813b0291edf5ada7d
Apple <apple@Apples-MacBook-Pro-63.local>
2019-06-24 16:44:17 -0500

    Start

---
+++ toDoList2/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoList2
+//
+//  Created by Apple on 6/24/19.
+//  Copyright © 2019 Anika. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
